01decd541267b01be3cc5541f0c05eb19e8534d4
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-13 21:32:08 -0400

    This feels clean for now

--- user/user_handler.go
+++ user/user_handler.go
@@ -14,8 +14,14 @@ func NewUserHandler(userService *UserService) *UserHandler {
 	return &UserHandler{UserService: userService}
 }
 
-func (h *UserHandler) GetUser(c *gin.Context) {
+func (h *UserHandler) GetUsers(c *gin.Context) {
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (h *UserHandler) GetUserByID(c *gin.Context) {
 	id := c.Param("id")
+
 	user := h.UserService.GetUser(id)
+
 	c.JSON(http.StatusOK, gin.H{"user": user})
 }
